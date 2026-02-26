807ceeff61bacafb6ed650a11a2b5898903f15dc
sherlyn <sherlynlopezvillanueva@gmail.com>
2026-02-26 15:08:19 -0600

    Modificacion final Evaluacion

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
 
-      <h1>Vite + React</h1>
+      <h1>Vite + React +</h1>
 
       {/*  aqui se pone el nombre */}
       <p>Hola mundo soy sherlyn y este es mi primer proyecto</p>
